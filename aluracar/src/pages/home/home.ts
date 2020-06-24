fe619bc096909d8dcdfd27d7f80ccbbc4ad06d9f
Paulo Henrique Bendazzoli <pbendazzoli@ciandt.com>
2020-06-24 16:52:30 -0300

    Buscando os carros no servidor

--- aluracar/src/pages/home/home.ts
+++ aluracar/src/pages/home/home.ts
@@ -1,32 +1,49 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController, AlertController } from 'ionic-angular';
+import { HttpErrorResponse } from '@angular/common/http';
 
 import { Carro } from '../../modelos/carro';
+import { CarrosServiceProvider } from '../../providers/carros-service/carros-service';
+import { NavLifeCycles } from '../../utils/ionic/nav/nav-lifecycles';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
+export class HomePage implements NavLifeCycles{
 
   public carros: Carro[];
 
-  constructor(public navCtrl: NavController) {
-    this.carros = [
-      {nome: 'Azera V6', preco:85000},
-      {nome: 'Onix 1.6', preco:35000},
-      {nome: 'Fiesta 2.0', preco:52000},
-      {nome: 'C3 1.0', preco:22000},
-      {nome: 'Uno Fire', preco:11000},
-      {nome: 'Sentra 2.0', preco:53000},
-      {nome: 'Astra Sedan', preco:39000},
-      {nome: 'Vectra 2.0 Turbo', preco:37000},
-      {nome: 'Hilux 4x4', preco:90000},
-      {nome: 'Montana Cabine Dupla', preco:57000},
-      {nome: 'Outlander 2.4', preco:99000},
-      {nome: 'Brasília Amarela', preco:9500},
-      {nome: 'Omega Hatch', preco:8000}
-    ];
+  constructor(public navCtrl: NavController,
+              private _loadingCtrl: LoadingController,
+              private _alertCtrl: AlertController,
+              private _carrosServiceProvider: CarrosServiceProvider) {}
+  
+  ionViewDidLoad(): void {
+    let loading = this._loadingCtrl.create({
+      content: 'Carregando carros...'
+    });
+
+    loading.present();
+
+    this._carrosServiceProvider
+        .lista()
+        .subscribe(
+          (carros) => {
+            this.carros = carros;
+            loading.dismiss();
+          },
+          (err: HttpErrorResponse) => {
+            loading.dismiss();
+            this._alertCtrl.create({
+              title: 'Falha na conexão.',
+              subTitle: 'Não foi possível carregar a lista de carros, tente novamente mais tarde!',
+              buttons: [{
+                text: 'OK'
+              }]
+            }).present();
+          }
+        );
   }
 
 }
